d6d78e42044273f0c0359187088294f0357badf6
Tom Tobar <cocdetombomb@gmail.com>
2022-11-18 18:04:47 -0600

    add octocat bg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import NavBar from "./components/NavBar";
 import { Routes, Route } from 'react-router-dom'
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path='/home' element={}/>
+        <Route path='/home' element={<Home />}/>
       </Routes>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
+
+
 function Home() {
   return (
-    <div>Home</div>
+    <div class="not-able-to-do-this" style={{
+        width: "100vw",
+        height: "100vh",
+        display: "grid",
+        placeItems: "center",
+        backgroundImage: "url('https://avatars.githubusercontent.com/u/583231?v=4')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
+    }}>Welcome!</div>
   )
 }
 export default Home
